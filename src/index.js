0ab825547f2d0eca70a52a4b55c8b47840f4973b
Sebastian Werner <s.werner@sebastian-software.de>
2018-12-11 12:48:37 +0100

    Moved processing from Import => CallExpression. This fixes priority issues when working together with plugins which affect a more top-level node like https://github.com/smooth-code/loadable-components babel plugin.

--- src/index.js
+++ src/index.js
@@ -20,6 +20,17 @@ function hashString(input, precision = DEFAULT_LENGTH) {
     .slice(0, precision)
 }
 
+function collectImportCallPaths(startPath) {
+  const imports = []
+  startPath.traverse({
+    Import: function Import(importPath) {
+      imports.push(importPath)
+    }
+  })
+
+  return imports
+}
+
 function getImportArgPath(path) {
   return path.parentPath.get("arguments")[0]
 }
@@ -39,99 +50,102 @@ function getSimplifiedPrefix(request) {
   return simplified
 }
 
-export default function smartWebpackImport({ types, template }) {
-  const visited = Symbol("visited")
+const visited = Symbol("visited")
 
+function processImport(path, state) {
+  if (path[visited]) {
+    return
+  }
+  path[visited] = true
+
+  const importArg = getImportArgPath(path)
+  const importArgNode = importArg.node
+  const { quasis, expressions, leadingComments } = importArgNode
+
+  const requester = dirname(state.file.opts.filename)
+  const request = quasis ? quasis[0].value.cooked : importArgNode.value
+
+  // There exists the possibility of non usable value. Typically only
+  // when the user has import() statements with other complex data, but
+  // not a string or template string. We handle this gracefully by ignoring.
+  if (request == null) {
+    return
+  }
+
+  const jsonContent = {}
+
+  // Try to parse all previous comments
+  if (leadingComments) {
+    leadingComments.forEach((comment, index) => {
+      // Skip empty comments
+      if (!comment.value.trim()) {
+        return
+      }
+
+      // Webpack magic comments are declared as JSON5 but miss the curly braces.
+      let parsed
+      try {
+        parsed = json5.parse(`{${comment.value}}`)
+      } catch (err) {
+        // Most probably a non JSON5 comment
+        return
+      }
+
+      // Skip comment processing if it already contains a chunk name
+      if (parsed.webpackChunkName) {
+        jsonContent.webpackChunkName = true
+        return
+      }
+
+      // We copy over all fields and...
+      for (const key in parsed) {
+        jsonContent[key] = parsed[key]
+      }
+
+      // Cleanup the parsed comment afterwards
+      comment.value = ""
+    })
+  }
+
+  if (!jsonContent.webpackChunkName) {
+    const hasExpressions = expressions && expressions.length > 0
+
+    // Append [request] as placeholder for dynamic part in WebpackChunkName
+    const fullRequest = hasExpressions ? `${request}[request]` : request
+
+    // Prepend some clean identifier of the static part when using expressions.
+    // This is not required to work, but helps users to identify different chunks.
+    const requestPrefix = hasExpressions ? getSimplifiedPrefix(request) : ""
+
+    // Cleanup combined request to not contain any paths info
+    const plainRequest = basename(fullRequest, extname(fullRequest))
+
+    // Normalize requester between different OSs
+    const normalizedRequester = relative(root, requester)
+      .split(sep)
+      .join("/")
+
+    // Hash request origin and request
+    const importHash = hashString(`${normalizedRequester}::${request}`)
+
+    // Add our chunk name to the previously parsed values
+    jsonContent.webpackChunkName = `${requestPrefix}${plainRequest}-${importHash}`
+
+    // Convert to string and remove outer JSON object symbols {}
+    const magicComment = json5.stringify(jsonContent).slice(1, -1)
+
+    // Add as a new leading comment
+    importArg.addComment("leading", magicComment)
+  }
+}
+
+export default function smartWebpackImport({ types, template }) {
   return {
     name: "smart-webpack-import",
     visitor: {
-      Import(path, state) {
-        /* eslint-disable immutable/no-mutation */
-
-        if (path[visited]) {
-          return
-        }
-        path[visited] = true
-
-        const importArg = getImportArgPath(path)
-        const importArgNode = importArg.node
-        const { quasis, expressions, leadingComments } = importArgNode
-
-        const requester = dirname(state.file.opts.filename)
-        const request = quasis ? quasis[0].value.cooked : importArgNode.value
-
-        // There exists the possibility of non usable value. Typically only
-        // when the user has import() statements with other complex data, but
-        // not a string or template string. We handle this gracefully by ignoring.
-        if (request == null) {
-          return
-        }
-
-        const jsonContent = {}
-
-        // Try to parse all previous comments
-        if (leadingComments) {
-          leadingComments.forEach((comment, index) => {
-            // Skip empty comments
-            if (!comment.value.trim()) {
-              return
-            }
-
-            // Webpack magic comments are declared as JSON5 but miss the curly braces.
-            let parsed
-            try {
-              parsed = json5.parse(`{${comment.value}}`)
-            } catch (err) {
-              // Most probably a non JSON5 comment
-              return
-            }
-
-            // Skip comment processing if it already contains a chunk name
-            if (parsed.webpackChunkName) {
-              jsonContent.webpackChunkName = true
-              return
-            }
-
-            // We copy over all fields and...
-            for (const key in parsed) {
-              jsonContent[key] = parsed[key]
-            }
-
-            // Cleanup the parsed comment afterwards
-            comment.value = ""
-          })
-        }
-
-        if (!jsonContent.webpackChunkName) {
-          const hasExpressions = expressions && expressions.length > 0
-
-          // Append [request] as placeholder for dynamic part in WebpackChunkName
-          const fullRequest = hasExpressions ? `${request}[request]` : request
-
-          // Prepend some clean identifier of the static part when using expressions.
-          // This is not required to work, but helps users to identify different chunks.
-          const requestPrefix = hasExpressions ? getSimplifiedPrefix(request) : ""
-
-          // Cleanup combined request to not contain any paths info
-          const plainRequest = basename(fullRequest, extname(fullRequest))
-
-          // Normalize requester between different OSs
-          const normalizedRequester = relative(root, requester)
-            .split(sep)
-            .join("/")
-
-          // Hash request origin and request
-          const importHash = hashString(`${normalizedRequester}::${request}`)
-
-          // Add our chunk name to the previously parsed values
-          jsonContent.webpackChunkName = `${requestPrefix}${plainRequest}-${importHash}`
-
-          // Convert to string and remove outer JSON object symbols {}
-          const magicComment = json5.stringify(jsonContent).slice(1, -1)
-
-          // Add as a new leading comment
-          importArg.addComment("leading", magicComment)
-        }
+      CallExpression(path, state) {
+        const imports = collectImportCallPaths(path)
+        imports.forEach((importCall) => processImport(importCall, state))
       }
     }
   }
